0fa2badc683b2341364613c0089160805fb6918d
yudhardiansyah <yudhaardians@gmail.com>
2023-06-27 09:50:12 +0700

    bugfix

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ $(document).ready(function () {
     }
   });
 
+  //Click event to scroll to top
+  $('.scrollToTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
   // Banner Carousel
   $('.hero-banner').slick({
     infinite: true,
